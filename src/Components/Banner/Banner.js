e2da3e6abbd293b12c567552070aef2f16730b27
thwahatkp <thwahatkp@gmail.com>
2022-10-18 01:01:24 +0530

    Added Dynamic content on Banner using Axios

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import axios from '../../axioss'
+import { API_KEY , imageUrl} from '../../Constants/Constants'
 import './Banner.css'
 function Banner() {
+  const [movie, setMovie] = useState()
+  useEffect(()=>{
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0]);
+      setMovie(response.data.results[4])
+    })
+
+  },[])
   return (
-    <div className='banner'>
+    <div
+    style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""} )`}}
+     className='banner'>
+      {/* <img src={movie ? imageUrl+movie.backdrop_path : ""} alt="Loading..." /> */}
         <div className="content">
-            <h1 className="title">Movie Name</h1>
+            <h1 className="title">{movie ? movie.name : ""}</h1>
             <div className="bannerButtons">
                 <button className="button">Play</button>
                 <button className="button">My List</button>
             </div>
-            <h1 className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae voluptatum aut cupiditate reiciendis, laudantium iste aspernatur. Tempora vero nobis labore.</h1>
+            <h1 className="description">{movie ? movie.overview : ""}</h1>
         </div>
         <div className="fadeBottom"></div>
 
---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = '2f7a8e405981d44c3a41505713ee0e37'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
